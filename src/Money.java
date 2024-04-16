06d471433c9a0d951d6315583ad506da44e8121e
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-16 13:08:35 +0200

    Tercer commit con los delta en los test

--- src/Money.java
+++ src/Money.java
@@ -1,9 +1,22 @@
 public class Money {
 
-    public static float change(TipoMoneda origen, TipoMoneda destino, float money ) {
-        float moneyConverted = 0;
-
+    public static float change(TipoMoneda origen, TipoMoneda destino, float money) {
+        float moneyConverted;
+        if (money > 0) {
+            if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.USD)) {
+                moneyConverted = money * 1.18798F;
+            } else if(origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.EUR)) {
+                moneyConverted = money * 0.841815F;
+            } else if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.GBP)) {
+                moneyConverted = money * 0.857839F;
+            } else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.EUR)) {
+                moneyConverted = money * 1.165826F;
+            } else {
+                moneyConverted = -1F;
+            }
+        } else {
+            moneyConverted = -1F;
+        }
         return moneyConverted;
     }
-
 }
--- src/MoneyTest.java
+++ src/MoneyTest.java
@@ -5,41 +5,46 @@ import static org.junit.jupiter.api.Assertions.*;
 class MoneyTest {
     @Test
     void moneyTest1() {
-        assertEquals(28.37F, Money.change(TipoMoneda.EUR, TipoMoneda.USD, 23.88F));
+        assertEquals(28.37F, Money.change(TipoMoneda.EUR, TipoMoneda.USD, 23.88F), 0.2);
     }
 
     @Test
     void MoneyTest2() {
-        assertEquals(1165.83F, Money.change(TipoMoneda.GBP, TipoMoneda.EUR, 1000.0F));
+        assertEquals(1165.83F, Money.change(TipoMoneda.GBP, TipoMoneda.EUR, 1000.0F), 0.2);
     }
 
     @Test
     void MoneyTest3() {
-        assertEquals(201.21F, Money.change(TipoMoneda.EUR, TipoMoneda.GBP, 234.56F));
+        assertEquals(201.21F, Money.change(TipoMoneda.EUR, TipoMoneda.GBP, 234.56F), 0.2);
     }
 
     @Test
     void moneyTest4() {
-        assertEquals(37.51F, Money.change(TipoMoneda.USD, TipoMoneda.EUR, 44.56F));
+        assertEquals(37.51F, Money.change(TipoMoneda.USD, TipoMoneda.EUR, 44.56F), 0.2);
     }
 
     @Test
     void moneyTest5() {
-        assertEquals(138.49, Money.change(TipoMoneda.GBP, TipoMoneda.USD, 100.0F));
+        assertEquals(138.49, Money.change(TipoMoneda.GBP, TipoMoneda.USD, 100.0F), 0.2);
     }
 
     @Test
     void moneyTest6() {
-        assertEquals(722.14F,Money.change(TipoMoneda.USD, TipoMoneda.GBP, 1000.0F));
+        assertEquals(722.14F,Money.change(TipoMoneda.USD, TipoMoneda.GBP, 1000.0F), 0.2);
     }
 
     @Test
     void moneyTest7() {
+        assertEquals(-1F, Money.change(TipoMoneda.PTS, TipoMoneda.EUR, 100.0F));
+    }
+
+    @Test
+    void moneyTest8() {
         assertEquals(-1F, Money.change(TipoMoneda.EUR, TipoMoneda.PTS, 123.3F));
     }
 
     @Test
-    void moneyTest8 () {
+    void moneyTest9() {
         assertEquals(-1F, Money.change(TipoMoneda.USD, TipoMoneda.EUR, -167.34F));
     }
 }
