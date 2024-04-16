942c1dd48d01ec42556aa09172ab7f3bc53da944
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-16 13:37:54 +0200

    Quinto commit añadiendo las constantes al metodo change

--- src/Money.java
+++ src/Money.java
@@ -1,28 +1,33 @@
 public class Money {
 
     public static float change(TipoMoneda origen, TipoMoneda destino, float money) {
+        float constEURaUSD = 1.18798F;
+        float constUSDaEUR = 0.841815F;
+        float constEURaGBP = 0.857839F;
+        float constGBPaEUR = 1.165826F;
+        float constError = -1F;
         float moneyConverted;
         float moneyConvertedEur;
         if (money > 0) {
             if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.USD)) {
-                moneyConverted = money * 1.18798F;
+                moneyConverted = money * constEURaUSD;
             } else if(origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.EUR)) {
-                moneyConverted = money * 0.841815F;
+                moneyConverted = money * constUSDaEUR;
             } else if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.GBP)) {
-                moneyConverted = money * 0.857839F;
+                moneyConverted = money * constEURaGBP;
             } else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.EUR)) {
-                moneyConverted = money * 1.165826F;
+                moneyConverted = money * constGBPaEUR;
             } else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.USD)) {
-                moneyConvertedEur = money * 1.165826F; //Pasem primer a euros els GBP
-                moneyConverted = moneyConvertedEur * 1.18798F;
+                moneyConvertedEur = money * constGBPaEUR; //Pasem primer a euros els GBP
+                moneyConverted = moneyConvertedEur * constEURaUSD;
             }else if (origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.GBP)) {
-                moneyConvertedEur = money * 0.841815F;
-                moneyConverted = moneyConvertedEur * 0.857839F;
+                moneyConvertedEur = money * constUSDaEUR;
+                moneyConverted = moneyConvertedEur * constEURaGBP;
             } else {
-                moneyConverted = -1F;
+                moneyConverted = constError;
             }
         } else {
-            moneyConverted = -1F;
+            moneyConverted = constError;
         }
         return moneyConverted;
     }
