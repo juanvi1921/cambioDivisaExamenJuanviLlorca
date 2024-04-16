e9a93ddf2d7cf4f7dd300460eec6c2725762e93c
dam_lmj <juanvi1921.cursos@gmail.com>
2024-04-16 13:19:57 +0200

    Cuarto commit con la clase money terminada y los test pasados

--- src/Money.java
+++ src/Money.java
@@ -2,6 +2,7 @@ public class Money {
 
     public static float change(TipoMoneda origen, TipoMoneda destino, float money) {
         float moneyConverted;
+        float moneyConvertedEur;
         if (money > 0) {
             if (origen.equals(TipoMoneda.EUR) && destino.equals(TipoMoneda.USD)) {
                 moneyConverted = money * 1.18798F;
@@ -11,6 +12,12 @@ public class Money {
                 moneyConverted = money * 0.857839F;
             } else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.EUR)) {
                 moneyConverted = money * 1.165826F;
+            } else if (origen.equals(TipoMoneda.GBP) && destino.equals(TipoMoneda.USD)) {
+                moneyConvertedEur = money * 1.165826F; //Pasem primer a euros els GBP
+                moneyConverted = moneyConvertedEur * 1.18798F;
+            }else if (origen.equals(TipoMoneda.USD) && destino.equals(TipoMoneda.GBP)) {
+                moneyConvertedEur = money * 0.841815F;
+                moneyConverted = moneyConvertedEur * 0.857839F;
             } else {
                 moneyConverted = -1F;
             }
